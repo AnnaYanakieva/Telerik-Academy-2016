e0dc9528bfc2f538b684f4f76c39c24e3011f2a8
AnnaYanakieva <a.ianakieva@gmail.com>
2016-04-01 22:47:36 +0300

    Create PrintFirstName.cs

---
+++ CSharp1/01.Intro-Programming/PrintFirstName.cs
@@ -0,0 +1,12 @@
+using System;
+
+class PrintFirstName
+{
+    static void Main()
+    {
+        string firstName = "First Name";
+        string lastName = "Last Name";
+        Console.WriteLine(firstName + "\n" + lastName);
+
+    }
+}
