990f4d94857d493d3711ac5c92745f8cc20e505f
AnnaYanakieva <a.ianakieva@gmail.com>
2016-04-01 23:01:53 +0300

    Update and rename Age.cs to 15.Age.cs

--- CSharp1/01.Intro-Programming/15.Age.cs
+++ CSharp1/01.Intro-Programming/15.Age.cs
@@ -1,3 +1,6 @@
+//Write a program that reads your birthday(in the format MM.DD.YYYY) from the console 
+//and prints on the console how old you are you now and how old will you be after 10 years.
+
 using System;
 
 class Age
